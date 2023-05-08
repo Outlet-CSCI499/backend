e351aea77dfa0b6d8804652707a6e10f8e233ff0
Tyler Robinson <tylerrhb11@gmail.com>
2023-05-08 17:42:50 -0400

    using axios instead of request for newscatcher api

--- routes/news.js
+++ routes/news.js
@@ -1,20 +1,18 @@
 // endpoints for news stuff will go here
 
-const express = require("express")
-const router = express.Router()
+var axios = require("axios").default;
 
-const request = require('request');
-const options = {
-    method: 'GET',
-    url: 'https://newscatcher.p.rapidapi.com/v1/sources',
-    qs: {topic: 'tech', lang: 'en', country: 'USA'},
-    headers: {
-        'x-rapidapi-key': '7PM1muSncLVX_lypeasOh0Nnf3aZD-dCq6mYkwOkW0M',
-        'x-rapidapi-host': 'newscatcher.p.rapidapi.com',
-        useQueryString: true
-    }
+var options = {
+  method: 'GET',
+  url: 'https://api.newscatcherapi.com/v2/search',
+  params: {q: 'Bitcoin', lang: 'en', topic: 'tech', sort_by: 'relevancy', page: '1'},
+  headers: {
+    'x-api-key': '7PM1muSncLVX_lypeasOh0Nnf3aZD-dCq6mYkwOkW0M'
+  }
 };
-request(options, function (error, response, body) {
-    if (error) throw new Error(error);
-    console.log(body);
+
+axios.request(options).then(function (response) {
+	console.log(response.data);
+}).catch(function (error) {
+	console.error(error);
 });
