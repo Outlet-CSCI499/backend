65ca6464c18c0e0fe6992c97e63c22719fa3bc00
Tyler Robinson <tylerrhb11@gmail.com>
2023-05-08 17:39:42 -0400

    newscatcher api key and call added

--- routes/news.js
+++ routes/news.js
@@ -1,1 +1,20 @@
 // endpoints for news stuff will go here
+
+const express = require("express")
+const router = express.Router()
+
+const request = require('request');
+const options = {
+    method: 'GET',
+    url: 'https://newscatcher.p.rapidapi.com/v1/sources',
+    qs: {topic: 'tech', lang: 'en', country: 'USA'},
+    headers: {
+        'x-rapidapi-key': '7PM1muSncLVX_lypeasOh0Nnf3aZD-dCq6mYkwOkW0M',
+        'x-rapidapi-host': 'newscatcher.p.rapidapi.com',
+        useQueryString: true
+    }
+};
+request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+    console.log(body);
+});
