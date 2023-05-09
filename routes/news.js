809753e053804ec6c24b4e853f7a8109af201dd3
Tyler Robinson <tylerrhb11@gmail.com>
2023-05-09 13:42:12 -0400

    fixed api request

--- routes/news.js
+++ routes/news.js
@@ -5,7 +5,7 @@ var axios = require("axios").default;
 var options = {
   method: 'GET',
   url: 'https://api.newscatcherapi.com/v2/search',
-  params: {q: 'Bitcoin', lang: 'en', topic: 'tech', sort_by: 'relevancy', page: '1'},
+  params: {lang: 'en', topic: 'tech', sort_by: 'relevancy', page: '1'},
   headers: {
     'x-api-key': '7PM1muSncLVX_lypeasOh0Nnf3aZD-dCq6mYkwOkW0M'
   }
