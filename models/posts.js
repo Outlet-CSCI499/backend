3282416a133bfb916f465134a1300b1fe8b4caee
SheikhFuad <79721701+SheikhFuad@users.noreply.github.com>
2023-05-10 14:07:35 -0400

    Created the model for posts set the endpoints

--- models/posts.js
+++ models/posts.js
@@ -1,33 +1,97 @@
-const voting = {
-    pepperoni: 0,
-    cheese: 0,
-    hawaiian: 0,
-}
+const db = require("../db")
+const { NotFoundError} = require("../utils/errors")
 
-class Voting {
-
-    // get total votes of poll
-    static async tallyVotes() {
-        // handling calculating the final results for our poll
-        // and return those results
-
-        return voting
-    }
-
-    // increase tally for the pizza that was voted for
-    static async recordVote(pizzaName) {
-        // increment the pizza name that was voted for
-        // and return the final results
-
-        if (voting[pizzaName] || voting[pizzaName] === 0) {
-            voting[pizzaName] += 1
-        }
-
-        return Voting.tallyVotes()
-    }
-
+class Post {
+  constructor({authorId, title, body, upvote, downvote, created, edited}) {
+    //All the required objects for posts
+    this.authorId = authorId;
+    this.title = title;
+    this.body = body;
+    this.upvote = upvote;
+    this.downvote = downvote;
+    this.created = created;
+    this.edited = edited;
     
+  }
+
+  static async find() {
+    // Fetch all posts 
+    const query = `
+      SELECT author_id, title, body, upvote, downvote, created, edited
+      FROM posts
+      ORDER BY created_at DESC
+    `;
+    const result = await db.query(query);
+    return result.rows.map(row => new Post(row));
+  }
+
+  static async fetchPostByauthorId(authorId) {
+    //Fetch all posts by author of the post
+    //Throw an error if author information is missing or not found
+    const query = `
+      SELECT author_id, title, body, upvote, downvote, created, edited
+      FROM posts
+      WHERE authorID = $1
+    `;
+    const result = await db.query(query, [authorId]);
+    if (result.rows.length === 0) {
+      throw new NotFoundError(`Post containing the ID ${authorID} not found`);
+    }
+    return new Post(result.rows[0]);
+  }
+
+  static async create({ title, body, authorId }) {
+    // create a new post that requires the title, body, and the name of the author
+    const query = `
+      INSERT INTO posts (title, body, author_id)
+      VALUES ($1, $2, $3)
+      SET created = NOW()
+      RETURNING author_id, title, body, upvote, downvote, created, edited
+    `;
+    const result = await db.query(query, [title, body, authorId]);
+    return new Post(result.rows[0]);
+  }
+
+  async edit() {
+    // Edit an existing post and return the edited post
+    const query = `
+      UPDATE posts
+      SET title = $2, body = $3, edited= NOW()
+      WHERE id = $1
+      RETURNING author_id, title, body, upvote, downvote, created, edited
+    `;
+    const result = await db.query(query, [this.authorID, this.title, this.body]);
+    this.updatedAt = result.rows[0].edited;
+    return this;
+  }
+
+  async removePost() {
+    //delete an exixting post 
+    const query = `
+      DELETE FROM posts
+      WHERE authorID = $1
+    `;
+    await db.query(query, [this.authorID]);
+  }
+
+   async upvote(userName) {
+    //upvote a specific post
+    const query = `
+        INSERT INTO upvote (author_ID, user_ID)
+        VALUES ($1, $2)
+    `;
+    await db.query(query, [this.authorId, userName]);    
+
+   }
+   async downvote(userName) {
+    //downvote a specific post
+    const query = `
+        INSERT INTO downvote (author_ID, user_ID)
+        VALUES ($1, $2)
+    `;
+    await db.query(query, [this.authorId, userName]);    
+
+   }
 
 }
-
-module.exports = Voting
+module.exports = Post;
--- routes/posts.js
+++ routes/posts.js
@@ -1,4 +1,128 @@
 // endpoints for posts will go here
-const express = require("express")
-const router = express.Router()
-const User = require("../models/posts")
+const express = require('express');
+const Post = require('../models/posts');
+const router = express.Router();
+const { NotFoundError, BadRequestError, UnauthorizedError} = require("../utils/errors")
+
+// Create a new post
+router.post('/', async (req, res, next) => {
+  const {title, body, authorId} = req.body;
+  try {
+    const post = await Post.create({title, body, authorId})
+    res.status(201).json({ post });
+    //throw exception if the post is empty 
+    if (!title || !body || !authorId ){
+        throw new BadRequestError ("Title, Author, and Content missing")
+    }
+  } 
+  catch (err) {
+    next(err);
+  }
+});
+
+// Get all posts from database
+router.get('/', async (req, res, next) => {
+  try {
+    const allposts = await Post.find();
+    res.status(200).json({ allposts });
+  } 
+  catch (err) {
+    next(err);
+  }
+});
+
+// aquire a specific post by author
+router.get('/:authorId', async (req, res, next) => {
+  const {authorId} = req.params.authorId;
+  try {
+    const post = await Post.fetchPostByauthorId(authorId)
+    // throw an error if author is not found
+    if (!authorId) {
+        throw new NotFoundError ("User not found")
+    }
+    res.status(200).json({ post });
+  } 
+  catch (err) {
+    next(err);
+  }
+});
+
+// edit a specific post by a specific author
+router.patch('/:authorId', async (req, res, next) => {
+  const author = req.params.authorId;
+  const {title, body, authorId} = req.body;
+  try {
+    const post = await Post.fetchPostByauthorId(author);
+    //update the new content 
+    post.title = title||post.title;
+    post.body = body||post.body;
+    post.authorId = authorId||post.authorId;
+    const edited = await post.edit()
+    //throw an error if the editor is not the original author 
+    if (!post.authorId) {
+      throw new UnauthorizedError("You don't have permission to edit");
+    //throw an error if the post does not exist 
+    }else if(!post){
+      throw new NotFoundError ("User not found")
+    }
+    res.status(201).json({edited});
+  } 
+  catch (err) {
+    next(err);
+  }
+});
+
+// Delete a specific post by a specific author
+router.delete('/:authorId', async (req, res, next) => {
+  const authorId = req.params.authorId;
+  try {
+    const post = await Post.fetchPostByauthorId(authorId);
+    //throw an error if the editor is not the original author 
+    if (post.authorId !== authorId) {
+      throw new UnauthorizedError("You don't have permission to delete");
+    }
+    await post.removePost();
+    res.status(204).json({post});
+  } 
+  catch (err) {
+    next(err);
+  }
+});
+
+// Upvote a specific post by a specific author
+router.post('/:authorId/upvote', async (req, res, next) => {
+  const authorId = req.params.authorId;
+  const userID = req.body.userID
+  try {
+    const post = await Post.fetchPostByauthorId(authorId);
+    //throw an error if the post does not exist 
+    if (!post) {
+      return res.status(404).json({ err: 'Post not found' });
+    }
+    await post.upvote(userID);
+    res.status(204).json({post});
+  } 
+  catch (err) {
+    next(err);
+  }
+});
+
+// Downvote a specific post by a specific author
+router.post('/:authorId/downvote', async (req, res, next) => {
+  const authorId = req.params.authorId;
+  const userID = req.body.userID
+  try {
+    const post = await Post.fetchPostByauthorId(authorId);
+    //throw an error if the post does not exist 
+    if (!post) {
+      return res.status(404).json({ err: 'Post not found' });
+    }
+    await post.downvote(userID);
+    res.status(204).json({post});
+  } 
+  catch (err) {
+    next(err);
+  }
+});
+
+  module.exports = router;
