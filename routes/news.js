48fa7b4bf26b0c60359c3c0fce20ffebffb53a64
henrycheung1 <henrycheung1234567890@gmail.com>
2023-05-12 21:32:19 -0400

    updated news.js

--- routes/news.js
+++ routes/news.js
@@ -1,27 +1,34 @@
 // endpoints for news stuff will go here
 
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
 var axios = require("axios").default;
 
 var options = {
-  method: 'GET',
-  url: 'https://api.newscatcherapi.com/v2/search',
-  params: {lang: 'en', topic: 'tech', sort_by: 'date', page: '1'},
+  method: "GET",
+  url: "https://api.newscatcherapi.com/v2/latest_headlines",
+  params: {
+    lang: "en",
+    countries: "US",
+    topic: "tech",
+    when: "14d",
+    page_size: "100",
+  },
   headers: {
-    'x-api-key': '7PM1muSncLVX_lypeasOh0Nnf3aZD-dCq6mYkwOkW0M'
-  }
+    "x-api-key": "7PM1muSncLVX_lypeasOh0Nnf3aZD-dCq6mYkwOkW0M",
+  },
 };
 
-
 router.get("/news", async (req, res, next) => {
-  
-  axios.request(options).then(function (response) {
-    console.log(response.data);
-    const articles = response.data;
-    res.status(200).json({ articles });
-  }).catch(function (error) {
-    console.error(error);
-  });
-})
+  await axios
+    .request(options)
+    .then(function (response) {
+      console.log(response.data);
+      const articles = response.data;
+      res.status(200).json({ articles });
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+});
