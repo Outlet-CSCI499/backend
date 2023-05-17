fee83c97e6fe88dc378776c8bca8fe6ec68839f8
henrycheung1 <henry.cheung1@yahoo.com>
2023-05-17 00:53:51 -0400

    Updated post.js

--- models/posts.js
+++ models/posts.js
@@ -1,37 +1,37 @@
-const db = require("../db")
-const { NotFoundError} = require("../utils/errors")
+const db = require("../db");
+const { NotFoundError } = require("../utils/errors");
 
 class Post {
-  constructor({authorId, title, body, upvote, downvote, created, edited}) {
+  constructor({ id, user_id, title, body, upvote, downvote, created, edited }) {
     //All the required objects for posts
-    this.authorId = authorId;
+    this.id = id;
+    this.authorId = user_id;
     this.title = title;
     this.body = body;
     this.upvote = upvote;
     this.downvote = downvote;
     this.created = created;
     this.edited = edited;
-    
   }
 
   static async find() {
-    // Fetch all posts 
+    // Fetch all posts
     const query = `
-      SELECT author_id, title, body, upvote, downvote, created, edited
+      SELECT id, user_id, title, body, upvote, downvote, created, edited
       FROM posts
-      ORDER BY created_at DESC
+      ORDER BY created DESC
     `;
     const result = await db.query(query);
-    return result.rows.map(row => new Post(row));
+    return result.rows.map((row) => new Post(row));
   }
 
   static async fetchPostByauthorId(authorId) {
     //Fetch all posts by author of the post
     //Throw an error if author information is missing or not found
     const query = `
-      SELECT author_id, title, body, upvote, downvote, created, edited
+      SELECT id, user_id, title, body, upvote, downvote, created, edited
       FROM posts
-      WHERE authorID = $1
+      WHERE user_id = $1
     `;
     const result = await db.query(query, [authorId]);
     if (result.rows.length === 0) {
@@ -40,13 +40,27 @@ class Post {
     return new Post(result.rows[0]);
   }
 
+  static async fetchPostBypostId(postId) {
+    //Fetch all posts by author of the post
+    //Throw an error if author information is missing or not found
+    const query = `
+      SELECT id, user_id, title, body, upvote, downvote, created, edited
+      FROM posts
+      WHERE id = $1
+    `;
+    const result = await db.query(query, [postId]);
+    if (result.rows.length === 0) {
+      throw new NotFoundError(`Post containing the ID ${postId} not found`);
+    }
+    return new Post(result.rows[0]);
+  }
+
   static async create({ title, body, authorId }) {
     // create a new post that requires the title, body, and the name of the author
     const query = `
-      INSERT INTO posts (title, body, author_id)
+      INSERT INTO posts (title, body, user_id)
       VALUES ($1, $2, $3)
-      SET created = NOW()
-      RETURNING author_id, title, body, upvote, downvote, created, edited
+      RETURNING id, user_id, title, body, upvote, downvote, created, edited
     `;
     const result = await db.query(query, [title, body, authorId]);
     return new Post(result.rows[0]);
@@ -58,40 +72,66 @@ class Post {
       UPDATE posts
       SET title = $2, body = $3, edited= NOW()
       WHERE id = $1
-      RETURNING author_id, title, body, upvote, downvote, created, edited
+      RETURNING user_id, title, body, upvote, downvote, created, edited
     `;
-    const result = await db.query(query, [this.authorID, this.title, this.body]);
+    const result = await db.query(query, [
+      this.authorID,
+      this.title,
+      this.body,
+    ]);
     this.updatedAt = result.rows[0].edited;
     return this;
   }
 
+  // async removePost() {
+  //   //delete an exixting post
+  //   const query = `
+  //     DELETE FROM posts
+  //     WHERE user_id = $1
+  //   `;
+  //   await db.query(query, [this.authorId]);
+  // }
+
   async removePost() {
-    //delete an exixting post 
+    //delete an exixting post
     const query = `
       DELETE FROM posts
-      WHERE authorID = $1
+      WHERE id = $1
     `;
-    await db.query(query, [this.authorID]);
+    await db.query(query, [this.id]);
   }
 
-   async upvote(userName) {
+  // async upvote(userName) {
+  //   //upvote a specific post
+  //   const query = `
+  //       INSERT INTO upvote (user_id, user_ID)
+  //       VALUES ($1, $2)
+  //   `;
+  //   await db.query(query, [this.authorId, userName]);
+  // }
+  // async downvote(userName) {
+  //   //downvote a specific post
+  //   const query = `
+  //       INSERT INTO downvote (user_id, user_ID)
+  //       VALUES ($1, $2)
+  //   `;
+  //   await db.query(query, [this.authorId, userName]);
+  // }
+
+  static async upvote(postId) {
     //upvote a specific post
     const query = `
-        INSERT INTO upvote (author_ID, user_ID)
-        VALUES ($1, $2)
+      UPDATE posts SET upvote = upvote+1 WHERE id = $1
     `;
-    await db.query(query, [this.authorId, userName]);    
+    await db.query(query, [postId]);
+  }
 
-   }
-   async downvote(userName) {
+  static async downvote(postId) {
     //downvote a specific post
     const query = `
-        INSERT INTO downvote (author_ID, user_ID)
-        VALUES ($1, $2)
+    UPDATE posts SET downvote = downvote+1 WHERE id = $1
     `;
-    await db.query(query, [this.authorId, userName]);    
-
-   }
-
+    await db.query(query, [postId]);
+  }
 }
 module.exports = Post;
--- routes/posts.js
+++ routes/posts.js
@@ -1,128 +1,170 @@
 // endpoints for posts will go here
-const express = require('express');
-const Post = require('../models/posts');
+const express = require("express");
+const Post = require("../models/posts");
 const router = express.Router();
-const { NotFoundError, BadRequestError, UnauthorizedError} = require("../utils/errors")
+const {
+  NotFoundError,
+  BadRequestError,
+  UnauthorizedError,
+} = require("../utils/errors");
 
 // Create a new post
-router.post('/posts', async (req, res, next) => {
-  const {title, body, authorId} = req.body;
+router.post("/posts", async (req, res, next) => {
+  const { title, body, authorId } = req.body;
   try {
-    const post = await Post.create({title, body, authorId})
+    const post = await Post.create({ title, body, authorId });
     res.status(201).json({ post });
-    //throw exception if the post is empty 
-    if (!title || !body || !authorId ){
-        throw new BadRequestError ("Title, Author, and Content missing")
+    //throw exception if the post is empty
+    if (!title || !body || !authorId) {
+      throw new BadRequestError("Title, Author, and Content missing");
     }
-  } 
-  catch (err) {
+  } catch (err) {
     next(err);
   }
 });
 
 // Get all posts from database
-router.get('/posts', async (req, res, next) => {
+router.get("/posts", async (req, res, next) => {
   try {
     const allposts = await Post.find();
     res.status(200).json({ allposts });
-  } 
-  catch (err) {
+  } catch (err) {
     next(err);
   }
 });
 
 // aquire a specific post by author
-router.get('/posts/:authorId', async (req, res, next) => {
-  const {authorId} = req.params.authorId;
+router.get("/posts/:authorId", async (req, res, next) => {
+  const { authorId } = req.params.authorId;
   try {
-    const post = await Post.fetchPostByauthorId(authorId)
+    const post = await Post.fetchPostByauthorId(authorId);
     // throw an error if author is not found
     if (!authorId) {
-        throw new NotFoundError ("User not found")
+      throw new NotFoundError("User not found");
     }
     res.status(200).json({ post });
-  } 
-  catch (err) {
+  } catch (err) {
     next(err);
   }
 });
 
 // edit a specific post by a specific author
-router.patch('/posts/:authorId', async (req, res, next) => {
+router.patch("/posts/:authorId", async (req, res, next) => {
   const author = req.params.authorId;
-  const {title, body, authorId} = req.body;
+  const { title, body, authorId } = req.body;
   try {
     const post = await Post.fetchPostByauthorId(author);
-    //update the new content 
-    post.title = title||post.title;
-    post.body = body||post.body;
-    post.authorId = authorId||post.authorId;
-    const edited = await post.edit()
-    //throw an error if the editor is not the original author 
+    //update the new content
+    post.title = title || post.title;
+    post.body = body || post.body;
+    post.authorId = authorId || post.authorId;
+    const edited = await post.edit();
+    //throw an error if the editor is not the original author
     if (!post.authorId) {
       throw new UnauthorizedError("You don't have permission to edit");
-    //throw an error if the post does not exist 
-    }else if(!post){
-      throw new NotFoundError ("User not found")
+      //throw an error if the post does not exist
+    } else if (!post) {
+      throw new NotFoundError("User not found");
     }
-    res.status(201).json({edited});
-  } 
-  catch (err) {
+    res.status(201).json({ edited });
+  } catch (err) {
     next(err);
   }
 });
+
+// // Delete a specific post by a specific author
+// router.delete("/posts/:authorId", async (req, res, next) => {
+//   const authorId = req.params.authorId;
+//   try {
+//     const post = await Post.fetchPostByauthorId(authorId);
+//     //throw an error if the editor is not the original author
+//     console.log(post.authorId, authorId);
+//     if (post.authorId != authorId) {
+//       throw new UnauthorizedError("You don't have permission to delete");
+//     }
+//     await post.removePost();
+//     res.status(204).json({ post });
+//   } catch (err) {
+//     next(err);
+//   }
+// });
 
 // Delete a specific post by a specific author
-router.delete('/posts/:authorId', async (req, res, next) => {
-  const authorId = req.params.authorId;
+router.delete("/posts/:postId", async (req, res, next) => {
+  const authorId = req.params.postId;
   try {
     const post = await Post.fetchPostByauthorId(authorId);
-    //throw an error if the editor is not the original author 
-    if (post.authorId !== authorId) {
-      throw new UnauthorizedError("You don't have permission to delete");
-    }
     await post.removePost();
-    res.status(204).json({post});
-  } 
-  catch (err) {
+    res.status(204).json({ post });
+  } catch (err) {
     next(err);
   }
 });
 
+// // Upvote a specific post by a specific author
+// router.post("/posts/:authorId/upvote", async (req, res, next) => {
+//   const authorId = req.params.authorId;
+//   const userID = req.body.userID;
+//   try {
+//     const post = await Post.fetchPostByauthorId(authorId);
+//     //throw an error if the post does not exist
+//     if (!post) {
+//       return res.status(404).json({ err: "Post not found" });
+//     }
+//     await post.upvote(userID);
+//     res.status(204).json({ post });
+//   } catch (err) {
+//     next(err);
+//   }
+// });
+
+// // Downvote a specific post by a specific author
+// router.post("/posts/:authorId/downvote", async (req, res, next) => {
+//   const authorId = req.params.authorId;
+//   const userID = req.body.userID;
+//   try {
+//     const post = await Post.fetchPostByauthorId(authorId);
+//     //throw an error if the post does not exist
+//     if (!post) {
+//       return res.status(404).json({ err: "Post not found" });
+//     }
+//     await post.downvote(userID);
+//     res.status(204).json({ post });
+//   } catch (err) {
+//     next(err);
+//   }
+// });
+
 // Upvote a specific post by a specific author
-router.post('/posts/:authorId/upvote', async (req, res, next) => {
-  const authorId = req.params.authorId;
-  const userID = req.body.userID
+router.post("/posts/:postId/upvote", async (req, res, next) => {
+  const postId = req.params.postId;
   try {
-    const post = await Post.fetchPostByauthorId(authorId);
-    //throw an error if the post does not exist 
+    const post = await Post.fetchPostBypostId(postId);
+    //throw an error if the post does not exist
     if (!post) {
-      return res.status(404).json({ err: 'Post not found' });
+      return res.status(404).json({ err: "Post not found" });
     }
-    await post.upvote(userID);
-    res.status(204).json({post});
-  } 
-  catch (err) {
+    await Post.upvote(postId);
+    res.status(204).json({ post });
+  } catch (err) {
     next(err);
   }
 });
 
 // Downvote a specific post by a specific author
-router.post('/posts/:authorId/downvote', async (req, res, next) => {
-  const authorId = req.params.authorId;
-  const userID = req.body.userID
+router.post("/posts/:postId/downvote", async (req, res, next) => {
+  const postId = req.params.postId;
   try {
-    const post = await Post.fetchPostByauthorId(authorId);
-    //throw an error if the post does not exist 
+    const post = await Post.fetchPostBypostId(postId);
+    //throw an error if the post does not exist
     if (!post) {
-      return res.status(404).json({ err: 'Post not found' });
+      return res.status(404).json({ err: "Post not found" });
     }
-    await post.downvote(userID);
-    res.status(204).json({post});
-  } 
-  catch (err) {
+    await Post.downvote(postId);
+    res.status(204).json({ post });
+  } catch (err) {
     next(err);
   }
 });
 
-  module.exports = router;
+module.exports = router;
